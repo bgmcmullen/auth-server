0c4c1eb1a05b8ed988b8d88e92e96af0c38718a6
Brendan <bgmcmullen@comcast.net>
2024-05-07 17:40:37 -0400

    errors corrected

--- src/auth/middleware/basic.js
+++ src/auth/middleware/basic.js
@@ -1,17 +1,21 @@
 'use strict';
 
 const base64 = require('base-64');
-const { user } = require('../models/index.js');
+const { users } = require('../models/index.js');
 
 module.exports = async (req, res, next) => {
 
   if (!req.headers.authorization) { return _authError(); }
 
+  
   let basic = req.headers.authorization;
-  let [username, pass] = base64.decode(basic).split(':');
+  let [authType, authString] = basic.split(' ')
+
+  let decodedString = base64.decode(authString);
+  let [username, pass] = decodedString.split(':');
 
   try {
-    req.user = await user.authenticateBasic(username, pass)
+    req.user = await users.authenticateBasic(username, pass)
     next();
   } catch (e) {
     console.error(e);
--- src/auth/router/handlers.js
+++ src/auth/router/handlers.js
@@ -2,14 +2,17 @@
 
 const { users } = require('../models/index.js');
 
+const bcrypt = require('bcrypt');
+
 async function handleSignup(req, res, next) {
+
   try {
     let userRecord = await users.create(req.body);
     const output = {
       user: userRecord,
       token: userRecord.token
     };
-    res.status(200).json(output);
+    res.status(201).json(output);
   } catch (e) {
     console.error(e);
     next(e);
@@ -19,8 +22,8 @@ async function handleSignup(req, res, next) {
 async function handleSignin(req, res, next) {
   try {
     const user = {
-      user: request.user,
-      token: request.user.token
+      user: req.user,
+      token: req.user.token
     };
     res.status(200).json(user);
   } catch (e) {
@@ -31,8 +34,8 @@ async function handleSignin(req, res, next) {
 
 async function handleGetUsers(req, res, next) {
   try {
-    const userRecords = await Users.findAll({});
-    const list = users.map(user => user.username);
+    const userRecords = await users.findAll({});
+    const list = userRecords.map(user => user.username);
     res.status(200).json(list);
   } catch (e) {
     console.error(e);
@@ -40,8 +43,9 @@ async function handleGetUsers(req, res, next) {
   }
 }
 
+// text changed to send
 function handleSecret(req, res, next) {
-  res.status(200).text("Welcome to the secret area!");
+  res.status(200).send("Welcome to the secret area!");
 }
 
 module.exports = {
